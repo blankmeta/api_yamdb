81c5f6cf4159ca4fb548b75ec1e56ab104699bad
Мария Степанова <mstepanova188@gmail.com>
2022-11-03 13:58:45 +0300

    Вьюхи

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -14,3 +14,14 @@ class IsAuthor(permissions.BasePermission):
 class AdminOrSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == 'admin' or request.user.is_superuser
+
+
+class AuthorOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
