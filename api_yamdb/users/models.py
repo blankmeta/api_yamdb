982ff9387cce9098036d631dbd28644bc20661b9
blankmeta <yadanilfilatov@gmail.com>
2022-11-08 21:16:48 +0300

    user change role_permission

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -19,6 +19,21 @@ class User(AbstractUser):
         default='user',
         max_length=10,
     )
+    is_admin = models.BooleanField(default=False, )
+    is_moderator = models.BooleanField(default=False, )
+
+    def change_role_permission(self, new_role):
+        role_to_permission_field = {
+            'admin': self.is_admin,
+            'moderator': self.is_moderator
+        }
+        for role_permission in role_to_permission_field.values():
+            role_permission = False
+        if new_role in role_to_permission_field:
+            role_to_permission_field[new_role] = True
+
+    def save(self, *args, **kwargs):
+        self.change_role_permission(self.role)
 
     class Meta:
         ordering = ['id']
