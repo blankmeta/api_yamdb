59f425f45bb958516d48f64cb954b79f070bb86b
blankmeta <yadanilfilatov@gmail.com>
2022-11-12 17:08:18 +0300

    Some code style

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -56,18 +56,15 @@ class TitleViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = (IsAdminOrReadOnly | IsModerator | IsAuthor,)
-    
+
     def get_title(self):
-        title_id = self.kwargs['title_id']
-        title = get_object_or_404(Title, pk=title_id)
-        return title
+        return get_object_or_404(Title, pk=self.kwargs['title_id'])
 
     def get_queryset(self):
         return self.get_title().reviews.all()
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user,
-                        title=self.get_title())
+        serializer.save(author=self.request.user, title=self.get_title())
 
 
 class CommentViewSet(viewsets.ModelViewSet):
